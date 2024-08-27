625c50f0118db499c5fe59895fcdf1ab1981e300
Padmac1985 <Padmac1985@gmail.com>
2024-08-27 19:05:22 -0400

    Print natural numbers

---
+++ printnaturalnumbers.java
@@ -0,0 +1,11 @@
+public  class printnaturalnumbers {
+    public static void main (String[] args) {
+        int y=0;
+
+  for(int x=15; x>=1; x--) {
+    System.out.println("my value is " + x);
+    y=y+x;
+
+  } System.out.println("the sum is "+y);
+    }
+}
